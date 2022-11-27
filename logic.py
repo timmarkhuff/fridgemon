51e55bb56f44dbfea05c7fa5c42b61e1dd39970e
timmarkhuff <89954856+timmarkhuff@users.noreply.github.com>
2022-11-26 19:26:36 -0800

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,160 @@
+from tkinter import PhotoImage
+from datetime import datetime, timedelta
+import random
+from time import sleep
+from timeit import default_timer as timer
+from threading import Thread
+import platform
+
+# OS-specific imports and initializations
+OS = platform.system()
+if OS == 'Linux':
+    import gpiozero
+    rpi_button = gpiozero.Button(4)
+else:
+    import keyboard
+
+SECONDS_PER_DAY = 30 # num of actual seconds per simulated day
+FOOD_LABELS = ['Steak', 'Cake', 'Salmon', 'Xiao Long Bao', 'Spaghetti',
+            'Linguine', 'Bulgogi', 'Stew', 'Halibut', 'Burger', 'Gumbo',
+            'Chow Mein', 'Pork Chop', 'Soup', 'Sushi', 'Chicken Parm',
+            'Lasagna', 'Fajita', 'Tacos', 'Carne Asada', 'Fried Rice',
+            'Pancakes', 'Rice Cakes']
+
+def simulate_today():
+    """
+    simulates the passage of time at a faster rate
+    for simulation purposes
+    """
+    return datetime.today() + timedelta(days=timer()/SECONDS_PER_DAY)
+
+class FridgeMon:
+    def __init__(self):
+        # create Tag objects from csv
+        self.tags = {}
+        with open('tags.csv') as f:
+            lines = f.readlines()
+            lines = lines[1:] # remove the column header
+            for line in lines:
+                line_split = line.split(',')
+                id = line_split[0]
+                name = line_split[1]
+                color = line_split[2]
+                icon_path = line_split[3]
+                tag = Tag(self, id, name, color, icon_path)
+                self.tags[id] = tag
+                
+                # skip empty rows
+                if line[0] == ',':
+                    continue
+
+                # randomly label some tags as being in fridge
+                if random.randint(0,2):
+                    # choose a random food label
+                    label = random.choice(FOOD_LABELS)
+
+                    # choose a random start_date
+                    today = simulate_today()
+                    rand_time_delta = timedelta(days = random.randint(0,5))
+                    start_date = today - rand_time_delta
+
+                    tag.add_to_fridge(start_date, label)
+                    self.tags[id].is_labeled = True
+
+    def scan_contents_simulated(self) -> tuple:
+        for tag in self.tags.values():
+            if not random.randint(0,3):
+                if tag.is_in_fridge:
+                    tag.remove_from_fridge()
+                else:
+                    tag.add_to_fridge(simulate_today())
+
+class Tag:
+    def __init__(self, fm, id: int, name: str, color: str, icon_path: str):
+        self.fm: FridgeMon = fm
+        self.id: int = id
+        self.name: str = name
+        self.color: str = color
+        self.icon_path: str = icon_path
+        self.icon: PhotoImage = None
+        self.label: str = ''    
+        self.is_in_fridge = False
+        self.is_labeled = False
+
+    def add_to_fridge(self, start_date: datetime, label: str = None) -> None:
+        self.is_in_fridge = True
+        self.label = label
+        self.start_date = start_date
+        self.start_time = timer()
+        self.is_labeled = False
+
+    def remove_from_fridge(self) -> None:
+        self.is_in_fridge = False
+        self.is_labeled = False
+
+    def label_contents(self, label:str) -> None:
+        self.label: str = label
+        self.is_labeled = True
+
+    def get_icon(self):
+        """
+        returns the icon for the tag
+        if the icon hasn't already been read from the filepath,
+        then it will be read
+        """
+        if self.icon is None:
+            self.icon = PhotoImage(file=self.icon_path)
+        return self.icon
+
+    def get_days_in_fridge(self):
+        """
+        simulated days
+        """
+        today = simulate_today()
+        return (today - self.start_date).days
+
+    def get_seconds_in_fridge(self):
+        """
+        actual seconds
+        """
+        return timer() - self.start_time 
+
+class DoorButton:
+    def __init__(self, ui, mon):
+        self.ui = ui
+        self.mon = mon
+        self.run = True
+        self.position = 0
+        self.positions = [0,0]
+
+    def start(self):
+        def thread():
+            x = 0
+            while self.run:
+                sleep(.05)
+                if OS == 'Linux':
+                    # for RPI
+                    self.position = rpi_button.is_pressed
+                else:
+                    self.position = keyboard.is_pressed('0')
+                    
+                # update the list of positions
+                self.positions.append(self.position)
+                self.positions = self.positions[1:]
+
+                if self.positions[0] == 0 and self.positions[1] == 1:
+                    text = "Door Open"
+                    fg = "#FF0000" # red
+                    self.ui.status_label.config(text=text, fg=fg)
+                if self.positions[0] == 1 and self.positions[1] == 0:
+                    text = "Door Closed" # green
+                    fg = "#84BD93"
+                    self.ui.status_label.config(text=text, fg=fg)
+                    self.mon.scan_contents_simulated()
+                    self.ui.refresh_item_buttons()
+                x += 1
+        Thread(target=thread).start()
+
+    def stop(self):
+        self.run = False
+
--- main.py
+++ main.py
@@ -1,109 +1,183 @@
 from tkinter import Tk, Frame, Label, Button, LEFT, RIGHT
-from classes import FridgeMon, Tag, DoorButton
+from logic import FridgeMon, DoorButton, FOOD_LABELS
+from threading import Thread
+from time import sleep
 
 BG = "#FEF9EF"
 TEXT_COLOR = '#73665C'
-master = Tk()
-master.configure(bg=BG)
-title = 'FridgeMon'
-master.title(title)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 400
-master.geometry(f"{SCREEN_WIDTH}x{SCREEN_HEIGHT}")
+REFRESH_TIME = 60 # seconds
 
-# define some frames and labels
-title_frame = Frame(master, padx=10, bg=BG)
-title_label = Label(title_frame, text='FridgeMon', font=('Inter', 30, 'bold'), bg=BG, fg=TEXT_COLOR)
-status_label = Label(title_frame, text='Door Closed', fg = "#84BD93", font=('Inter', 20), padx=10, bg=BG)
-text = 'Reduce waste and take control of your fridge!'
-tagline_label = Label(title_frame, text=text, font=('Inter', 12), bg=BG, fg=TEXT_COLOR)
-item_frame = Frame(master, padx=10, bg=BG)
+class UI:
+    def __init__(self, mon: FridgeMon):
+        self.mon = mon
+        self.master = Tk()
+        title = 'FridgeMon'
+        self.master.title(title)
+        self.master.configure(bg=BG)
+        self.master.geometry(f"{SCREEN_WIDTH}x{SCREEN_HEIGHT}")
+        self.run = True
 
-# grid the frames and labels
-title_frame.grid(row=0, column=0, sticky='w')
-title_label.grid(row=0, column=0, sticky='w')
-status_label.grid(row=0, column=1, sticky='w')
-tagline_label.grid(row=1, column=0, sticky='w')
-item_frame.grid(row=1, column=0, sticky='w')
+        # timer-related attributes
+        self.time_since_refresh = 0
+        self.start_timer()
 
-def refresh_item_buttons(item_frame: Frame, mon: FridgeMon):
-    # clear the current contents of the frame
-    for button in item_frame.winfo_children():
-        button.destroy()
+        # create the initial display
+        self.start_item_screen()
 
-    # re-grid the buttons 
-    tags_in_fridge = [tag for tag in mon.tags.values() if tag.is_in_fridge]
-    if not tags_in_fridge:
-        text='No items found in fridge.'
-        empty_label = Label(item_frame, text=text, font=('Inter', 15), padx=20, bg=BG, fg=TEXT_COLOR)
-        empty_label.grid(row=0, column=0)
+    def start_item_screen(self):
+        # define some frames and labels
+        self.main_frame = Frame(self.master, padx=10, bg=BG)
+        self.title_frame = Frame(self.main_frame, padx=10, bg=BG)
+        self.title_label = Label(self.title_frame, text='FridgeMon', font=('Inter', 30, 'bold'), bg=BG, fg=TEXT_COLOR)
+        self.status_label = Label(self.title_frame, text='Door Closed', fg = "#84BD93", font=('Inter', 20), padx=10, bg=BG)
+        text = 'Reduce waste and take control of your fridge!'
+        self.tagline_label = Label(self.title_frame, text=text, font=('Inter', 12), bg=BG, fg=TEXT_COLOR)
+        self.item_frame = Frame(self.main_frame, padx=10, bg=BG)
 
-    tags_in_fridge.sort(key=lambda tag: tag.start_date, reverse=False)
-    row = 0
-    column = 0
-    for tag in tags_in_fridge:
-        days_in_fridge = tag.get_days_in_fridge()
+        # grid the frames and labels
+        self.main_frame.grid(row=0, column=0, sticky='w')
+        self.title_frame.grid(row=0, column=0, sticky='w')
+        self.title_label.grid(row=0, column=0, sticky='w')
+        self.status_label.grid(row=0, column=1, sticky='w')
+        self.tagline_label.grid(row=1, column=0, sticky='w')
+        self.item_frame.grid(row=1, column=0, sticky='w')
 
-        # string representation of days_in_fridge
-        if days_in_fridge == 1:
-            days_in_fridge_str = f'1 day'
-        else:
-            days_in_fridge_str = f'{days_in_fridge} days'
+    def start_label_screen(self, tag):
+        # clear the screen
+        for i in self.main_frame.winfo_children():
+            i.destroy()
 
-        # color for the text
-        if days_in_fridge < 4:
-            foreground='#514e4e'
-        else:
-            foreground='#dc1f1f'
+        # create some labels
+        self.main_frame = Frame(self.master, padx=10, bg=BG)
+        text = f'Select a label for {tag.name}'
+        self.icon_label = Label(self.main_frame, text=text)
 
-        # the tag label is none, show "???"
-        if tag.label is None:
-            label = '???'
-        else:
-            label = tag.label
+        # grid the labels
+        self.main_frame.grid(row=0, column = 0)
+        self.icon_label.grid(row=0, column = 0)
 
-        button = Button(item_frame, 
-                        image=tag.get_icon(), 
-                        text=f'{days_in_fridge_str}\n{label}',
-                        background=tag.color,
-                        foreground=foreground,
-                        font=('Inter', 15, 'bold'),
-                        compound=RIGHT,
-                        padx=10,
-                        pady=10,
-                        width=232,
-                        anchor ='w',
-                        justify=LEFT,
-                        )
-        button.grid(column=column, row=row)
-        
-        # update column and row count
-        if column >= 2:
-            column = 0
+        row = 1
+        for label in FOOD_LABELS:
+            button = Button(self.main_frame, 
+                            text=label,
+                            font=('Inter', 12),
+                            background='#F5D3BB',
+                            width=15,
+                            command = lambda tag=tag, label=label: self.click_label_button(tag, label)
+                            )
+            button.grid(row=row, column=0, sticky='nsew')
             row += 1
-        else:
-            column += 1
 
-def print_removed_items(removed, added):
-    for i in removed:
-        print(f'{i.label} removed!')
-    for i in added:
-        print(f'{i.name} added!')
+    def refresh_item_buttons(self):
+        # clear the current contents of the frame
+        for button in self.item_frame.winfo_children():
+            button.destroy()
+
+        # sort the list of tags
+        tags_in_fridge = [tag for tag in self.mon.tags.values() if tag.is_in_fridge]
+        tags_in_fridge.sort(key=lambda tag: tag.start_date, reverse=False)
+        # show unlabel,recently added items first
+        show_first = [tag for tag in tags_in_fridge if tag.get_seconds_in_fridge() <= REFRESH_TIME and not tag.is_labeled]
+        # next, show items that were not recently added to fridge and all labeled items
+        show_next = [tag for tag in tags_in_fridge if tag.get_seconds_in_fridge() > REFRESH_TIME or tag.is_labeled]
+        tags_in_fridge = show_first + show_next
+
+        # add some default text if there are no tags
+        if not tags_in_fridge:
+            text='No items found in fridge.'
+            empty_label = Label(self.item_frame, text=text, font=('Inter', 15), padx=20, bg=BG, fg=TEXT_COLOR)
+            empty_label.grid(row=0, column=0)
+
+        # re-grid the buttons 
+        row = 0
+        column = 0
+        for tag in tags_in_fridge:
+            days_in_fridge = tag.get_days_in_fridge()
+
+            # the tag label is none, show "???"
+            if tag.label is None:
+                label = '???'
+            else:
+                label = tag.label
+
+            # text for the button
+            if tag in show_first:
+                button_text = f'Just added\nLabel it!'
+            elif days_in_fridge == 1:
+                button_text = f'1 day\n{label}'
+            else:
+                button_text = f'{days_in_fridge} days\n{label}'
+
+            # color for the text
+            if days_in_fridge < 4:
+                foreground='#514e4e'
+            else:
+                foreground='#dc1f1f'
+
+            # background color of the button
+            if tag in show_first:
+                color = '#84BD93'
+            else:
+                color = '#F5D3BB'
+
+            # create the food item button
+            button = Button(self.item_frame, 
+                            image=tag.get_icon(), 
+                            text=button_text,
+                            background=color,
+                            foreground=foreground,
+                            font=('Inter', 15, 'bold'),
+                            compound=RIGHT,
+                            padx=10,
+                            pady=10,
+                            width=232,
+                            anchor ='w',
+                            justify = LEFT,
+                            command = lambda tag=tag: self.start_label_screen(tag)
+                            )
+            button.grid(column=column, row=row)
+
+            # update column and row count
+            if column >= 2:
+                column = 0
+                row += 1
+            else:
+                column += 1
+
+    def click_label_button(self, tag, label:str):
+        self.main_frame.destroy()
+        tag.label_contents(label)
+        self.start_item_screen()
+        self.refresh_item_buttons()
+
+    def start_timer(self):
+        freq = .1 # seconds
+        def thread():
+            while self.run:
+                sleep(freq)
+                self.time_since_refresh += freq
+                if self.time_since_refresh > REFRESH_TIME:
+                    self.time_since_refresh = 0
+                    self.refresh_item_buttons()
+        Thread(target=thread).start()
+
+    def mainloop(self):
+        self.master.mainloop()
+
+    def stop(self) -> None:
+        self.run = False  
 
 mon = FridgeMon()
-cbk_funcs = (
-    (mon.scan_contents_simulated, None),
-    (print_removed_items, None),
-    (refresh_item_buttons, (item_frame, mon)),
-)
-door_button = DoorButton(status_label, cbk_funcs)
+ui = UI(mon)
+door_button = DoorButton(ui, mon)
+
+ui.refresh_item_buttons()
 door_button.start()
-
-# initialize the screen
-refresh_item_buttons(item_frame, mon)
-
-master.mainloop()
+ui.mainloop()
 
 # clean up
+ui.stop()
 door_button.stop()
 
